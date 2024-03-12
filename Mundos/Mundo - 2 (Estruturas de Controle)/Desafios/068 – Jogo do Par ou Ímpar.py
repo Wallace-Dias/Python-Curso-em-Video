e0864c73e8a81e0da14698f91d5e973407e10ad1
Wallace Medeiros Dias <wallacemedeiros.d@gmail.com>
2024-03-12 16:54:26 -0300

    Update 068 – Jogo do Par ou Ímpar.py

--- Mundos/Mundo - 2 (Estruturas de Controle)/Desafios/068 – Jogo do Par ou Ímpar.py
+++ Mundos/Mundo - 2 (Estruturas de Controle)/Desafios/068 – Jogo do Par ou Ímpar.py
@@ -1,2 +1,7 @@
 """ Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador perder, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo. """
+import random
 
+c = random.randint(1, 100)
+esc = ""
+
+print(c)
