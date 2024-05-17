2dd2ec5ef5e74b19b39b6293789018e543fc4054
Wallace Medeiros Dias <wallacemedeiros.d@gmail.com>
2024-05-17 16:28:58 -0300

    Desafio 82 - Concluído

--- Mundos/Mundo - 3 (Estrutura de Dados - Listas, Tulplas, Dicionários, Funções, Módulos e Pacotes)/Desafios/Desafios - mundo 3/082 – Dividindo valores em várias listas.py
+++ Mundos/Mundo - 3 (Estrutura de Dados - Listas, Tulplas, Dicionários, Funções, Módulos e Pacotes)/Desafios/Desafios - mundo 3/082 – Dividindo valores em várias listas.py
@@ -1,1 +1,23 @@
 """ Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas. """
+
+lista = []
+listb = []
+listc = []
+
+while True:
+    n = int(input("Digite um valor: "))
+    if n == 0:
+        break
+    lista.append(n)
+
+for i in lista:
+    if i % 2 == 0:
+        listb.append(i)
+    else:
+        listc.append(i)
+
+print(f'Primeira Lista: {lista}')
+
+print(f"Lista com Valores Pares: {listb}")
+
+print(f"Lista com Valores Impares: {listc}")
