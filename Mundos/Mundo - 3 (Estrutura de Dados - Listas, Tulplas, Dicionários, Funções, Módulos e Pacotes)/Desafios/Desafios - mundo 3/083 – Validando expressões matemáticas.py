e372a13f16880d0e7ebd7738eb0e5e1402e6baa7
Wallace Medeiros Dias <wallacemedeiros.d@gmail.com>
2024-05-18 15:46:24 -0300

    Desafio 083 - Concluído
    Adicionei na pasta arquivos do desafio mundo 3 um png do fluxograma do código pois me ajudou no entendimento dá lógica.

--- Mundos/Mundo - 3 (Estrutura de Dados - Listas, Tulplas, Dicionários, Funções, Módulos e Pacotes)/Desafios/Desafios - mundo 3/083 – Validando expressões matemáticas.py
+++ Mundos/Mundo - 3 (Estrutura de Dados - Listas, Tulplas, Dicionários, Funções, Módulos e Pacotes)/Desafios/Desafios - mundo 3/083 – Validando expressões matemáticas.py
@@ -1,6 +1,43 @@
 """ Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta. """
 
-expressão = str(input("Digite a expressão: "))
+
+n = str(input("Digite uma expressão: "))
+lista = []
+
+# Exemplo de expressão: ((((a+b)*c)+2)/4)
+
+for i in n:
+
+    if i == '(':
+        lista.append('(')
+        
+
+    elif i == ')':
+        if len(lista) > 0:
+            lista.pop()
+        else:
+            lista.append(')')
+            break
+
+if len(lista) == 0:
+    print("\nExpressão Válida!")
+else:
+    print("\nExpressão Inválida!")
+
+
+
+
+
+
+
+
+
+
+
+
+# Versão incorreta
+
+""" expressão = str(input("Digite a expressão: "))
 a = 0
 f = 0
 
@@ -16,4 +53,4 @@ if a == f:
     print(f'Abertos: {a} Fechados: {f}')
 else:
     print('Expressão Inválida! ')
-    print(f'Abertos: {a} Fechados: {f}')
+    print(f'Abertos: {a} Fechados: {f}') """
--- Mundos/Mundo - 3 (Estrutura de Dados - Listas, Tulplas, Dicionários, Funções, Módulos e Pacotes)/Desafios/Rascunho.py
+++ Mundos/Mundo - 3 (Estrutura de Dados - Listas, Tulplas, Dicionários, Funções, Módulos e Pacotes)/Desafios/Rascunho.py
@@ -1,4 +1,4 @@
-from time import sleep
+
 n = str(input("Digite algo: "))
 lista = []
 
@@ -6,18 +6,14 @@ for i in n:
     if i == '(':
         lista.append('(')
         
-        print(f"Foi adicionado ( a lista e o valor é: {len(lista)}")
     elif i == ')':
         if len(lista) > 0:
-            print(f" i foi == ) > 0 Len da Lista: {len(lista)}")
             lista.pop()
-            print(f"ultimo item removio Len da Lista: {len(lista)}")
+            
         else:
             lista.append(')')
-            print(f"i == ) == 0 Len da Lista: {len(lista)}")
             break
 
-print(f"Len final da lista: {len(lista)}")
 
 if len(lista) == 0:
     print("Sua expressão é válida")
