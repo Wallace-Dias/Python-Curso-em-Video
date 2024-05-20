64d375a6da1ad188409ee4e1b8fbbc40d5c33bf6
Wallace Medeiros Dias <wallacemedeiros.d@gmail.com>
2024-05-20 11:04:00 -0300

    Desafio - 084 Concluído

--- Mundos/Mundo - 3 (Estrutura de Dados - Listas, Tulplas, Dicionários, Funções, Módulos e Pacotes)/Desafios/Desafios - mundo 3/084 – Lista composta e análise de dados.py
+++ Mundos/Mundo - 3 (Estrutura de Dados - Listas, Tulplas, Dicionários, Funções, Módulos e Pacotes)/Desafios/Desafios - mundo 3/084 – Lista composta e análise de dados.py
@@ -5,25 +5,44 @@ A) Quantas pessoas foram cadastradas.
 B) Uma listagem com as pessoas mais pesadas.
 C) Uma listagem com as pessoas mais leves. """
 
-pessoas = []
-pesada = leve = tot = 0
-for c in range (0, 5):
-    nome = str(input("Nome: "))
-    peso = float(input("Peso: "))
-    pessoas.append(nome)
-    pessoas.append(peso)
+temp = [] # Exemplo temp = [nome[0] peso[1]]
+
+principal = [] # Exemplo principal = [[nome, peso] 0, [nome, peso] 1]
+maior = menor = tot = 0
+for c in range (0, 6):
+    temp.append(str(input("Nome: "))) # 0
+    temp.append(float(input("Peso: "))) # 1
+
+    if len(principal) == 0:
+        maior = menor = temp[1]
+
+    else:
+        if temp[1] > maior:
+            maior = temp[1]
+
+        if temp[1] < menor:
+            menor = temp[1]
+    principal.append(temp[:])
+    temp.clear()
+
     tot += 1
 
 # A) Quantas pessoas foram cadastradas:
 print(f"Foram cadastradas {tot} pessoas")
 
-#B) Lista de pessoas mais pessadas: 
-for c in pessoas:
-    if peso >= 100:
-        
-        print(f"Pessoas Pesadas:\n{c}")
-    else:
-        print(f'Pessoas Leves:\n{c}')
+#B) Lista de pessoas mais pessadas:
+print(f"O maior peso foi {maior}")
+for p in principal:
+    if p[1] == maior:
+        print(f'{p[0]}')
+
+print(f"O menor peso foi {menor}")
+for p in principal:
+    if p[1] == menor:
+        print(f"{p[0]}")
+
+
+    
 
     
 
