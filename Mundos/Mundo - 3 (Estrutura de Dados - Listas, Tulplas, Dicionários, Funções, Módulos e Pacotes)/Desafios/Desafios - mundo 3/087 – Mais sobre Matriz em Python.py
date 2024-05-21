c3966c3297a44ce1b1e051c627504f1c28c13f44
Wallace Medeiros Dias <wallacemedeiros.d@gmail.com>
2024-05-21 19:50:10 -0300

    correção descritiva

--- Mundos/Mundo - 3 (Estrutura de Dados - Listas, Tulplas, Dicionários, Funções, Módulos e Pacotes)/Desafios/Desafios - mundo 3/087 – Mais sobre Matriz em Python.py
+++ Mundos/Mundo - 3 (Estrutura de Dados - Listas, Tulplas, Dicionários, Funções, Módulos e Pacotes)/Desafios/Desafios - mundo 3/087 – Mais sobre Matriz em Python.py
@@ -1,5 +1,5 @@
 """Aprimore o desafio anterior, mostrando no final:
-A) A soma de todosos valores pares digitados.
+A) A soma de todos os valores pares digitados.
 B) A soma dos valores da terceira coluna.
 C) O maior valor da segunda linha. """
 
